f2925b5cad7ac8993aadc1cee0f6e45d9df4f4a1
saeedsuleimans <saeed.suleiman@outlook.com>
2023-12-19 17:27:52 +0000

    add variable

--- Iac/main.tf
+++ Iac/main.tf
@@ -7,51 +7,65 @@ terraform {
   }
 }
 
-provider "docker" {} 
+provider "docker" {}
+
 resource "docker_network" "demo_network" {
   name = var.network_name
 }
 
+# resource "docker_volume" "mssql_data_volume" {
+#   name   = var.volume_name
+#   driver = "local"
+# }
+
 resource "docker_image" "mssql_server" {
-    name  = "mcr.microsoft.com/mssql/server:2022-latest"
-    keep_locally = true
+  name         = "mcr.microsoft.com/mssql/server:2022-latest"
+  keep_locally = true
 }
 
 resource "docker_container" "mssql_container" {
-  name  = "mssql_container"
+  name  = var.mssql_container_name
   image = docker_image.mssql_server.name
-  env = ["ACCEPT_EULA=Y", "MSSQL_SA_PASSWORD=Password1!"]
+  env   = ["ACCEPT_EULA=Y", "MSSQL_SA_PASSWORD=Password1!"]
   ports {
     internal = 1433
     external = 1433
   }
-   network_mode = var.network_name
+  network_mode = var.network_name
+
+  # Mount the volume to persist data
+  volumes {
+    volume_name    = var.volume_name
+    container_path = var.volume_path
+    read_only      = false
+  }
 }
 
 resource "docker_image" "line_ten_api_image" {
-  name  = "lineten:10" 
+  name         = "lineten:10"
   keep_locally = true
 }
 
 resource "docker_container" "line_ten_api_container" {
-  name  = "line_ten_api_container"
+  name  = var.line_ten_container_name
   image = docker_image.line_ten_api_image.name
   ports {
     internal = 80
     external = 3000
   }
-  ports {
-    internal = 443
-    external = 3001
-  }
-  
   depends_on = [docker_container.mssql_container]
-  env = [ 
-    "ASPNETCORE_ENVIRONMENT=Development", 
-    "SQL_SERVER= mssql_container",
-    "SQL_USER=SA",  
-    "SQL_PASSWORD=Password1!" , 
-    "ConnectionStrings__dbConnectionString=Server=mssql_container;Database=TerraformDb;User=SA;Password=Password1!;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False" 
+  env = [
+    "ASPNETCORE_ENVIRONMENT=Development",
+    "SQL_SERVER=${docker_container.mssql_container.name}",
+    "SQL_USER=SA",
+    "SQL_PASSWORD=Password1!",
+    "ConnectionStrings__dbConnectionString=Server=${docker_container.mssql_container.name};Database=TerraformDb;User=SA;Password=Password1!;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"
   ]
   network_mode = var.network_name
+
+  # Mount the volume to access data from the MSSQL container
+  # volumes {
+  #   volume_name    = var.volume_name
+  #   container_path = var.volume_path
+  # }
 }
--- Iac/variables.tf
+++ Iac/variables.tf
@@ -1,5 +1,24 @@
-
 variable "network_name" {
-  default = "lineten_network"  
-  type = string
-} 
+  description = "Name of the Docker network"
+  default     = "demo_network"
+}
+
+variable "mssql_container_name" {
+  description = "Name of the MSSQL container"
+  default     = "mssql_container"
+}
+
+variable "line_ten_container_name" {
+  description = "Name of the LineTen API container"
+  default     = "line_ten_api_container"
+}
+
+variable "volume_name" {
+  description = "Name of the Docker volume"
+  default     = "database"
+}
+
+variable "volume_path" {
+  default     = "/var/opt/mssql/"
+  description = "Container volume path"
+}
